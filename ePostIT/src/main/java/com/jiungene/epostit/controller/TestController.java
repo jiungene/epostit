054cbf564faf191ad9b513f32fadf2c20c9e7997
Lim <vljh246v@naver.com>
2018-04-14 17:24:17 +0900

    -test controller 수정

--- ePostIT/src/main/java/com/jiungene/epostit/controller/TestController.java
+++ ePostIT/src/main/java/com/jiungene/epostit/controller/TestController.java
@@ -1,5 +1,35 @@
 package com.jiungene.epostit.controller;
 
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.jiungene.epostit.HomeController;
+
+@Controller
 public class TestController {
 
+
+	private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+	
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+		String formattedDate = dateFormat.format(date);
+
+		model.addAttribute("serverTime", formattedDate);
+
+		return "test";
+	}
 }
